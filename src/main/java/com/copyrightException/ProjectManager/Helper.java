5dfd9dbdc158198ff0ea8f08cfe0741caea06bd8
reloadit <raffael.apitz@hotmail.de>
2018-03-28 10:47:25 +0200

    login, register layout;
    on enter login; login layouting; register layouting; register & login error messages; Helper method to display error messages;

--- src/main/java/com/copyrightException/ProjectManager/Helper.java
+++ src/main/java/com/copyrightException/ProjectManager/Helper.java
@@ -2,6 +2,9 @@ package com.copyrightException.ProjectManager;
 
 import com.copyrightException.ProjectManager.entities.User;
 import com.copyrightException.ProjectManager.exceptions.NotLoggedInException;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 public class Helper {
@@ -20,4 +23,13 @@ public class Helper {
     public static void setUser(final User user) {
         UI.getCurrent().getSession().setAttribute(CURRENT_USER_PROPERTY, user);
     }
+
+    public static void displayErrorMessage(String title, String desc, Notification.Type type, Position pos, Page page) {
+        Notification notif = new Notification(title, desc, type);
+
+        notif.setPosition(pos);
+
+        notif.show(page);
+    }
+
 }
--- src/main/java/com/copyrightException/ProjectManager/views/login/LoginView.java
+++ src/main/java/com/copyrightException/ProjectManager/views/login/LoginView.java
@@ -5,17 +5,22 @@ import com.copyrightException.ProjectManager.entities.User;
 import com.copyrightException.ProjectManager.repositories.UserRepository;
 import com.copyrightException.ProjectManager.views.project.ProjectOverview;
 import com.copyrightException.ProjectManager.views.register.RegisterView;
-import com.vaadin.data.HasValue;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.navigator.View;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
@@ -29,7 +34,7 @@ public class LoginView extends VerticalLayout implements View {
     private static final Logger LOG = LoggerFactory.getLogger(LoginView.class);
     private final UserRepository userRepository;
     private Button bRegister, bLogin;
-    private Label lNameTitle, lPasswordTitle, lErrorName, lErrorPassword;
+    private Label lNameTitle, lPasswordTitle;
     private TextField tfName;
     private PasswordField pfPassword;
 
@@ -48,32 +53,29 @@ public class LoginView extends VerticalLayout implements View {
         pfPassword = new PasswordField();
         bLogin = new Button("Login");
         bRegister = new Button("Not registered yet?");
-        lErrorName = new Label();
-        lErrorPassword = new Label();
 
-        lErrorName.setVisible(false);
-        lErrorPassword.setVisible(false);
+        bLogin.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        bRegister.addStyleName(ValoTheme.BUTTON_PRIMARY);
 
         tfName.setPlaceholder("enter here");
         pfPassword.setPlaceholder("enter here");
 
-        tfName.addValueChangeListener(new HasValue.ValueChangeListener() {
+        tfName.addShortcutListener(new ShortcutListener("Shortcut Name", ShortcutAction.KeyCode.ENTER, null) {
             @Override
-            public void valueChange(HasValue.ValueChangeEvent event) {
-                lErrorName.setVisible(false);
+            public void handleAction(Object sender, Object target) {
+                onLogin();
             }
         });
-        pfPassword.addValueChangeListener(new HasValue.ValueChangeListener() {
+
+        pfPassword.addShortcutListener(new ShortcutListener("Shortcut Name", ShortcutAction.KeyCode.ENTER, null) {
             @Override
-            public void valueChange(HasValue.ValueChangeEvent event) {
-                lErrorPassword.setVisible(false);
+            public void handleAction(Object sender, Object target) {
+                onLogin();
             }
         });
 
         bLogin.setSizeFull();
         bRegister.setSizeFull();
-        lErrorName.setSizeFull();
-        lErrorPassword.setSizeFull();
     }
 
     private void initLayout() {
@@ -87,12 +89,9 @@ public class LoginView extends VerticalLayout implements View {
         grid.addComponent(pfPassword, 1, 1);
         grid.addComponent(bLogin, 1, 2);
         grid.addComponent(bRegister, 1, 4);
-        grid.addComponent(lErrorName, 2, 0);
-        grid.setComponentAlignment(lErrorName, Alignment.MIDDLE_CENTER);
-        grid.addComponent(lErrorPassword, 2, 1);
-        grid.setComponentAlignment(lErrorPassword, Alignment.MIDDLE_CENTER);
 
         addComponent(grid);
+        setSizeFull();
         setComponentAlignment(grid, Alignment.MIDDLE_CENTER);
     }
 
@@ -102,23 +101,18 @@ public class LoginView extends VerticalLayout implements View {
     }
 
     private void onLogin() {
-        System.out.println("onLogin");
-
         if (tfName.isEmpty()) {
-            lErrorName.setVisible(true);
-            lErrorName.setValue("please enter a username");
+            Helper.displayErrorMessage("Empty Username", "Please enter a username", Notification.Type.WARNING_MESSAGE, Position.TOP_CENTER, Page.getCurrent());
             return;
         }
 
         if (pfPassword.isEmpty()) {
-            lErrorPassword.setVisible(true);
-            lErrorPassword.setValue("please enter a password");
+            Helper.displayErrorMessage("Empty Password", "Please enter a password", Notification.Type.WARNING_MESSAGE, Position.TOP_CENTER, Page.getCurrent());
             return;
         }
 
         if (userRepository.findByName(tfName.getValue()).isEmpty()) {
-            lErrorName.setVisible(true);
-            lErrorName.setValue("this username does not exist");
+            Helper.displayErrorMessage("Invalid Username", "This username does not exist", Notification.Type.WARNING_MESSAGE, Position.TOP_CENTER, Page.getCurrent());
             return;
         }
 
@@ -132,16 +126,13 @@ public class LoginView extends VerticalLayout implements View {
                 Helper.setUser(user);
                 UI.getCurrent().getNavigator().navigateTo(ProjectOverview.VIEW_NAME);
             } else {
-                lErrorPassword.setVisible(true);
-                lErrorPassword.setValue("wrong password");
-                return;
+                Helper.displayErrorMessage("Wrong Password", "Please enter the correct password", Notification.Type.WARNING_MESSAGE, Position.TOP_CENTER, Page.getCurrent());
             }
         }
 
     }
 
     private void onRegister() {
-        System.out.println("onRegister");
         UI.getCurrent().getNavigator().navigateTo(RegisterView.VIEW_NAME);
     }
 }
