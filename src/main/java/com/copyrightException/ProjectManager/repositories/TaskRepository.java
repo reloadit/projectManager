60d2b9b54398e0409efb5802d01564e058ae20d4
Yatdu <jan.nicolas.ahrens@gmail.com>
2018-06-05 10:15:01 +0200

    Fehlende @Repository Annotation in dem TaskRepository hinzugefügt

--- src/main/java/com/copyrightException/ProjectManager/repositories/TaskRepository.java
+++ src/main/java/com/copyrightException/ProjectManager/repositories/TaskRepository.java
@@ -2,7 +2,9 @@ package com.copyrightException.ProjectManager.repositories;
 
 import com.copyrightException.ProjectManager.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, String> {
 
 }
